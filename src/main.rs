5b7112645be0fe41e4deecde0f86cac7ce4b79df
notplants <mfowler.email@gmail.com>
2021-05-19 11:55:33 +0200

    Add rocket.toml and peach-dyndns-server.service

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 extern crate rocket;
 
 use crate::routes::{index, register_domain, check_available};
+use rocket::Config;
+use rocket::figment::{Figment, Profile, providers::{Format, Toml, Serialized, Env}};
 
 mod cli;
 mod routes;
@@ -15,7 +17,12 @@ mod generate_zone;
 async fn main() {
     let _args = cli::args().expect("error parsing args");
 
-    let rocket_result = rocket::build()
+    // the following config says to use all default rocket configs
+    // and then override them with any configs specified in Rocket.toml
+    let config = Figment::from(rocket::Config::default())
+      .merge(Toml::file("Rocket.toml").nested());
+
+    let rocket_result = rocket::custom(config)
         .mount("/", routes![index, register_domain, check_available])
         .launch()
         .await;
